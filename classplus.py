b30060c9c420bedea706dcdd47961bf906df5033
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-03 22:06:06 +0530

    Update classplus.py

--- classplus.py
+++ classplus.py
@@ -90,7 +90,7 @@ async def account_login(bot: Client, m: Message):
 
     try:
 
-        reply = await message.chat.ask(
+        reply = await message.reply(
             (
                 '**'
                 'Send your credentials as shown below.\n\n'
