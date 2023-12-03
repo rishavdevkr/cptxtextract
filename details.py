b0297ddec8bbd5bc0bdd8395c8f0a66a8ac99568
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-03 20:59:48 +0530

    Create details.py

---
+++ details.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = 22779671
+api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
+bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+auth_users = [1713924419]
+sudo_user = 1713924419
+log_channel = -1002143629860
+txt_channel = -1002049391187
