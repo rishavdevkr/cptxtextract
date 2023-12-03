fd1e011464bb70777f82bf9c11d22d59e2b4b0ef
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-03 20:53:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import re
 
 import requests
 bot = Client(
-    "bot",
+    "CW",
     api_id= 22779671,
     api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
     bot_token= "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
