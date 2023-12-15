d72ff859c2bb2f798192e7bd0a63aca733487ed3
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-15 20:05:35 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -52,7 +52,7 @@ bot = Client(
     "bot",
     api_id= 22779671,
     api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
 )
 
 @bot.on_message(filters.command(["start"]))
