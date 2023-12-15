9d54a50d605fd33539060550f33318157da5129d
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2023-12-15 20:05:49 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 22779671
 api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c"
+bot_token = "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
 auth_users = [1713924419]
 sudo_user = 1713924419
 log_channel = -1002143629860
