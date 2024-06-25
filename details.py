cf3936932b4398e5a3db042aa8694798366c7b32
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2024-06-25 19:32:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,14 +3,14 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "0000000"))
-API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "000000000"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050200019 20000898623").split()))
+API_ID = int(getenv("API_ID", "25218674"))
+API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
+BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
+OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "999741495 20000898623").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002000006"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002038285084"))
 
 """
 #
