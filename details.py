16e19f715112b7881a80ad51c5bc71afacbdc901
Rishav Dev Kumar <69079321+rishavdevkr@users.noreply.github.com>
2024-06-25 19:53:11 +0530

    Update details.py

--- details.py
+++ details.py
@@ -3,7 +3,7 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "25218674"))
+API_ID = int(getenv("API_ID", 25218674))
 API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
 BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
 OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
